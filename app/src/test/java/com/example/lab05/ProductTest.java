77046caba12033d822ca753f2da7b4a4e0d52328
Hassan-khan389 <hkhan110@uottawa.ca>
2020-11-25 18:14:21 -0500

    added product test

---
+++ app/src/test/java/com/example/lab05/ProductTest.java
@@ -0,0 +1,31 @@
+package com.example.lab05;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ProductTest {
+
+    @Test
+    public void checkProductNames(){
+
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the name of the product", "DELL MONITOR", aProduct.getProductName());
+
+    }
+
+    @Test
+    public void checkProductId(){
+
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the id of the product", "1", aProduct.getId());
+
+    }
+
+    @Test
+    public void checkProductPrice(){
+
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+
+    }
+}
