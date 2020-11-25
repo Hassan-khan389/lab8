f7c9b7842031c1a2a90d01b37499ee4907029e54
Hassan-khan389 <hkhan110@uottawa.ca>
2020-11-25 17:37:58 -0500

    first commit

---
+++ app/src/main/java/com/example/lab05/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.lab05;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    EditText editTextPrice;
+    Button buttonAddProduct;
+    ListView listViewProducts;
+    DatabaseReference databaseProducts;
+
+    List<Product> products;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        listViewProducts = (ListView) findViewById(R.id.listViewProducts);
+        buttonAddProduct = (Button) findViewById(R.id.addButton);
+
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+        products = new ArrayList<>();
+
+        //adding an onclicklistener to button
+        buttonAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addProduct();
+            }
+        });
+
+        listViewProducts.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Product product = products.get(i);
+                showUpdateDeleteDialog(product.getId(), product.getProductName());
+                return true;
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //attaching value listener event
+        databaseProducts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                products.clear();
+                //iterating through all the nodes
+                //Any time you read data from the Database, you receive the data as a DataSnapshot
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting product
+                    Product product = postSnapshot.getValue(Product.class);
+                    products.add(product);
+                }
+                    //creating the adapter
+                    ProductList productsAdapter = new ProductList(MainActivity.this, products);
+                    //attaching adapter to the listview
+                    listViewProducts.setAdapter(productsAdapter);
+                }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void showUpdateDeleteDialog(final String productId, String productName) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+        final EditText editTextPrice  = (EditText) dialogView.findViewById(R.id.editTextPrice);
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateProduct);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteProduct);
+
+        dialogBuilder.setTitle(productName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+                if (!TextUtils.isEmpty(name)) {
+                    updateProduct(productId, name, price);
+                    b.dismiss();
+                }
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteProduct(productId);
+                b.dismiss();
+            }
+        });
+    }
+
+    private void updateProduct(String id, String name, double price) {
+        //getting the specified product reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+
+        //updating the product
+        Product product = new Product(id, name, price);
+        dR.setValue(product);
+        Toast.makeText(getApplicationContext(), "Product Updated",Toast.LENGTH_LONG).show();
+    }
+
+    private void deleteProduct(String id) {
+        //getting the specified product reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+        //removing product
+        dR.removeValue();
+        Toast.makeText(getApplicationContext(),"Product Deleted", Toast.LENGTH_LONG).show();
+    }
+    private void addProduct() {
+        //getting values to save
+        String name = editTextName.getText().toString().trim();
+        double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+
+        //checking if the value is provided
+        if(!TextUtils.isEmpty(name)){
+
+            //getting a unique id using push().getKey() method
+            //it will create a unique id and we will use it as the primary key for our product
+            String id = databaseProducts.push().getKey();
+
+            //creating a Product object
+            Product product = new Product(id, name, price);
+
+            //saving the product
+            databaseProducts.child(id).setValue(product);
+
+            //setting edittext to blank again
+            editTextName.setText("");
+            editTextPrice.setText("");
+
+            //displaying a success toast
+            Toast.makeText(this, "Product added", Toast.LENGTH_LONG).show();
+        }else{
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
+    }
+}
