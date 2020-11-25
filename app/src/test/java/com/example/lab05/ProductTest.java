e6aec890e9dca2d839cc95fb2f473a002865eff7
Hassan-khan389 <hkhan110@uottawa.ca>
2020-11-25 18:16:23 -0500

    fixed mistake

--- app/src/test/java/com/example/lab05/ProductTest.java
+++ app/src/test/java/com/example/lab05/ProductTest.java
@@ -25,7 +25,7 @@ public class ProductTest {
     public void checkProductPrice(){
 
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+        assertEquals("Check the price of the product", "180", aProduct.getPrice());
 
     }
 }
